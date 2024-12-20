8d60be65e052b30d06d1513f3b899b77163f052a
saikingalgulwad <saiprasadalgulwad@gmail.com>
2024-12-20 06:03:39 -0800

    first next js

---
+++ full-stack/app/pin-userid/[User]/page.jsx
@@ -0,0 +1,62 @@
+'use client'
+import React, { useState,useEffect } from 'react'
+import PinImage from '@/app/components/PinDetail/PinImage';
+import PinInfo from '@/app/components/PinDetail/PinInfo';
+import { HiArrowSmallLeft } from "react-icons/hi2";
+import { useRouter } from 'next/navigation';
+
+  function page({params}) {
+   
+const router = useRouter();
+
+
+      
+     
+      const [userInfo2 ,setUserInfo2]=useState();
+        useEffect(()=>{
+          if(params){
+            async function getdata() {
+              const data = await params;
+              
+            
+              getuserPin(data.User);
+            }
+            getdata();
+               
+          }
+    
+        },[params])
+    
+    
+    const getuserPin =async (email) =>{
+    
+      const res = await fetch(`http://localhost:3000/api/userId/${email}`,{cache:"no-store"});
+      const userPin = await res.json();
+      if(userPin===null){
+    
+      }else{
+        setUserInfo2(userPin.ok);
+     
+      }
+     console.log(userInfo2);
+    }
+  return (<>
+    {userInfo2? 
+      <div className=' bg-white p-3 md:p-12 rounded-2xl md:px-24 lg:px-36'>
+          <HiArrowSmallLeft className='text-[60px] font-bold ml-[-50px] 
+          cursor-pointer hover:bg-gray-200 rounded-full p-2 '
+          onClick={()=>router.back()}/>
+         <div className='grid grid-cols-1 lg:grid-cols-2 md:gap-10 shadow-lg
+         rounded-2xl p-3 md:p-7 lg:p-12 xl:pd-16 ' 
+         >
+        
+           <PinImage pinDetail={userInfo2} />
+           <div className="">
+           <PinInfo pinDetail={userInfo2}/>
+           </div>
+           </div>
+       </div>:null}</>
+  )
+}
+
+export default page
---
+++ full-stack/next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+
+    images:{
+        remotePatterns: [
+          
+            {
+           
+              hostname:'"res.cloudinary.com"',
+            },
+            {
+              protocol: 'https',
+              hostname: '"lh3.googleusercontent.com"',
+            },
+          ]
+    }
+
+};
+
+export default nextConfig;
