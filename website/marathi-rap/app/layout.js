69d8273a66474d3037d317a8c2cc3c2f4c71ba32
saikingalgulwad <167662574+saikingalgulwad@users.noreply.github.com>
2025-01-07 03:32:28 -0800

    change heading image for png

--- website/marathi-rap/app/layout.js
+++ website/marathi-rap/app/layout.js
@@ -13,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+    <link rel='icon' type='image/png' href='./logo1.png'/>
+    </head>
       <body
        className={outfit.className}
       >
